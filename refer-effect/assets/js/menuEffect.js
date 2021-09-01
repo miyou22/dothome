eee295572f5e564c3f385701e1c06c1cc74c9f3b
miyou22 <87642297+miyou22@users.noreply.github.com>
2021-09-01 21:26:22 +0900

    9.1

---
+++ refer-effect/assets/js/menuEffect.js
@@ -0,0 +1,7 @@
+$(".btn-wrap button").click(function(){
+    $("#modal").removeClass().addClass("show");
+  });
+  
+  $(".modal-cont button").click(function(){
+    $("#modal").addClass("hide")
+  });
