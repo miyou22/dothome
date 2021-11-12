a570bed41e39bcd4b48e5cc1f1c591f5dd54d5a9
miyou22 <87642297+miyou22@users.noreply.github.com>
2021-11-12 16:43:04 +0900

    11.12(4차 수정)

---
+++ site-php/include/footer.php
@@ -0,0 +1,65 @@
+<footer id="footer">
+    <div class="container">
+                <section class="section-service">
+                    <h2 class="ir_so">바로가기 링크</h2>
+                    <div class="link">
+                        <div>
+                            <strong>페이지</strong>
+                            <ul>
+                                <li><a href="main.php">main.php</a></li>
+                                <li><a href="../comment/comment.php">comment.php</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <strong>로그인</strong>
+                            <ul>
+                                <li><a href="../login/join.php">join.php</a></li>
+                                <li><a href="../login/joinSave.php">joinSave.php</a></li>
+                                <li><a href="../login/login.php">login.php</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <strong>게시판</strong>
+                            <ul>
+                                <li><a href="../board/board.php">board.php</a></li>
+                                <li><a href="../board/boardView.php">boardView.php</a></li>
+                                <li><a href="../board/boardWrite.php">boardWrite.php</a></li>
+                                <li><a href="../board/boardModify.php">boardModify.php</a></li>
+                                <li><a href="../board/boardSearch.php">boardSearch.php</a></li>
+                            </ul>
+                        </div>
+                        
+                        <div>
+                            <strong>사이트</strong>
+                            <ul>
+                                <li><a href="https://heoar.github.io/dothome21/" target="_blank">홈페이지</a></li>
+                                <li><a href="https://javab-png.tistory.com/" target="_blank">티스토리</a></li>
+                                <li><a href="https://github.com/heoar/dothome21.git" target="_blank">GITHUB</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <ul>
+                            <li>Contact</li>
+                            <li>yuooo13@gmail.com</li>
+                            <li>KaKao</li>
+                            <li>@yuooo13</li>
+                            <li>Home</li>
+                            <li>heoar.github.io/dothome21</li>
+                        </ul>
+                    </div>
+                </section>
+                <section class="section-info">
+                    <h2 class="ir_so">서비스 이용정도</h2>
+                    <address>&copy;HEOAR. All rights reserved.</address>
+                </section>
+            </div>
+            <a href="#" class="link_top">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" class="ico_top">
+                    <g fill="none" fill-rule="evenodd">
+                        <circle cx="25" cy="25" r="25" fill="black"></circle> 
+                        <path stroke-width="1.5" class="stroke" stroke="white" d="M8.467 16.933L0 8.467 8.467 0M.085 8.467L19.918 8.534" transform="rotate(90 9.5 24)"></path>
+                    </g>
+                </svg>
+            </a>
+</footer>
